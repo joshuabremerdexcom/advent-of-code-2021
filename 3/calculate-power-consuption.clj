5efefae207257b261da8f28e8b37a0ef71e758bb
Joshua Bremer <jbremer@dexcom.com>
2021-12-03 10:12:28 -0500

    Zipped the lists

---
+++ 3/calculate-power-consuption.clj
@@ -0,0 +1,35 @@
+(use 'clojure.java.io)
+(require '[clojure.zip :as zip])
+
+
+(defn get-lines [fname]
+  (with-open [r (reader fname)]
+    (doall (line-seq r))))
+
+(defn process-file-by-lines
+  "Process file reading it line-by-line"
+  ([file]
+   (process-file-by-lines file identity))
+  ([file process-fn]
+   (process-file-by-lines file process-fn println))
+  ([file process-fn output-fn]
+   (with-open [rdr (clojure.java.io/reader file)]
+     (doseq [line (line-seq rdr)]
+       (output-fn
+        (process-fn line))))))
+
+(defn read-file-by-lines
+  "Read file reading it line-by-line"
+  ([file]
+   (with-open [rdr (clojure.java.io/reader file)]
+     (map seq (map (line-seq rdr)))
+   )
+  )
+)
+
+
+(println (apply map str (map seq (get-lines "./3/simple_input.txt"))));; Will just print file line by ine
+;; (process-file-by-lines "./3/input.txt"
+;;                        reverse) ;; Will print each line reversed
+
+
---
+++ 3/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+clj ./3/calculate-power-consuption.clj
